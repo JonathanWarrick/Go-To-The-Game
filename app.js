41b0be1eccf1eaf6afb62d866fb8bf12cb07b74d
JonathanWarrick <jonathan.warrick@gmail.com>
2014-10-08 22:04:15 -0700

    Commit before massive delete of old work.

--- app.js
+++ app.js
@@ -2,16 +2,93 @@ angular.module('testApp', [
 	'ui.router'
 ])
 .controller('TestController', function($scope, $http, TeamData) {
+	// data that will be used to populate chart
 	$scope.data = {
 		labels: [],
 		series: []
 	};
 
+	// upon page load, get list of all teams in array and create object of win/loss
+	$scope.winLossData = {};
+	$scope.teamList = [];
+
+	$http.get('/api/getTeamData')
+	  .success(function(teamData) {
+	  	for (var team in teamData) {
+	  		var teamName = teamData[team].market + ' ' + teamData[team].teamName;
+	  		$scope.winLossData[teamName] = teamData[team].winPct;
+	  		teamList.push(teamName);
+	  	}
+	  	console.log('teamList is', $scope.teamList);
+	  	console.log('winLossData is', $scope.winLossData);
+	  });
+
+	$scope.winLossData;
+	$scope.convertedWinLossData = {};
+	
 	// console.log teamInfo to see if it works
 	$http.get('/api/getTeamData')
 	.success(function(teamData) {
-		console.log('here is the teamData', teamData);
+		$scope.winLossData = teamData;
+		for (var team in $scope.winLossData) {
+			// if ($scope.winLossData[team].market === "New England") {
+				var teamName = $scope.winLossData[team].market + ' ' + $scope.winLossData[team].teamName;
+				$scope.convertedWinLossData[teamName] = $scope.winLossData[team].winPct;
+			// }
+		}
+		console.log($scope.convertedWinLossData);
+		testFunction();
 	});
+	
+
+	
+	// for each teamInfo (getTeamInfo)
+	var testFunction = function() {
+		$scope.winLossData.forEach(function(team) {
+			if (team.market === 'New England' || team.market === 'Buffalo') {
+				console.log('team going into this loop is ', team);
+				// store team name and win percentage
+				var teamName = team.market + ' ' + team.teamName;
+				var winPct = team.winPct;
+
+				// console.log(teamName, 'teamName after conversion is');
+				// call getTeamInfo
+				var formattedString = 'http://api.seatgeek.com/2/events?performers[home_team].slug=' + teamName.toLowerCase().replace('.', '').split(' ').join('-');
+				console.log(formattedString);
+				$http.get(formattedString)
+				.success(function(data) {
+					console.log('got team data:', data);
+					var temp_avg_price = [];
+					var temp_labels = [];
+
+					data.events.forEach(function(event) {
+						temp_avg_price.push(event.stats.average_price);
+						var opponent;
+						event.performers.forEach(function(performer) {
+							if (!performer.home_team) {
+								opponent = performer.name;
+								if (opponent === 'St Louis Cardinals') {
+									opponent = 'St. Louis Cardinals';
+								}
+							}
+						});
+						var opponentWP = $scope.convertedWinLossData[opponent];
+						$scope.data2.labels.push((winPct + opponentWP) / 2);
+					});
+					$scope.data2.series.push(temp_avg_price);
+					// $scope.data2.labels.concat(temp_labels);
+				});
+			}
+		});
+		setTimeout(function() {
+			console.log($scope.data2);
+			Chartist.Line('.ct-chart', $scope.data2, {showLine: false});
+		}, 1000);
+	};
+
+
+
+
 
 	$scope.teamData = TeamData.teamData;
 
@@ -20,7 +97,7 @@ angular.module('testApp', [
 	$scope.getTeamInfo = function() {
 		$scope.data.labels = [];
 		$scope.data.series = [];
-		var formattedString = 'http://api.seatgeek.com/2/events?performers[home_team].slug=' + $scope.selectedTeam.name.toLowerCase().split(' ').join('-');
+		var formattedString = 'http://api.seatgeek.com/2/events?performers[home_team].slug=' + $scope.selectedTeam.name.toLowerCase().replace('.', '').split(' ').join('-');
 		$http.get(formattedString)
 		.success(function(data) {
 			console.log('got team data:', data);
@@ -64,7 +141,7 @@ factory('TeamData', function() {
 		{ name: "Tampa Bay Buccaneers"},
 		{ name: "Arizona Cardinals"},
 		{ name: "Seattle Seahawks"},
-		{ name: "St Louis Rams"},
+		{ name: "St. Louis Rams"},
 		{ name: "San Francisco 49ers"},
 		{ name: "Buffalo Bills"},
 		{ name: "New England Patriots"},
