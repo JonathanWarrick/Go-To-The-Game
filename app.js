d795a51d4591b9d1fc7f720c399c41b3887e11b6
JonathanWarrick <jonathan.warrick@gmail.com>
2014-10-11 19:43:58 -0700

    Adds working modal!

--- app.js
+++ app.js
@@ -65,26 +65,42 @@ angular.module('testApp', [
 			console.error('got an error sad', error);
 		});
 	};
-})
-.controller('DropdownCtrl', function ($scope) {
-  $scope.items = [
-    'The first choice!',
-    'And another choice for you.',
-    'but wait! A third!'
-  ];
+}).controller('ModalDemoCtrl', function ($scope, $modal, $log) {
 
-  $scope.status = {
-    isopen: false
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      backdrop: false,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+}).controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
   };
 
-  $scope.toggled = function(open) {
-    console.log('Dropdown is now: ', open);
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
   };
 
-  $scope.toggleDropdown = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-    $scope.status.isopen = !$scope.status.isopen;
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
   };
 });
-
